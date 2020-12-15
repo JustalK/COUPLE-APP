bf121da252916b994cb596ee0e3a013d1d53ff4a
Latsuj <justal.kevin@gmail.com>
2020-12-15 13:33:16 +0800

    [TEST] Install Jest

---
+++ src/App.test.tsx
@@ -0,0 +1,11 @@
+'use strict';
+
+import React from 'react';
+import renderer from 'react-test-renderer';
+import App from './App';
+
+describe('<App />', () => {
+	it('has 1 child', () => {
+		expect(true).toBe(true);
+	});
+});
