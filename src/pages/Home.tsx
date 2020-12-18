d9a3739d1218b21131da6725586f774f3ebe0db8
Latsuj <justal.kevin@gmail.com>
2020-12-18 21:43:20 +0800

    [REFACTORING] Removing the ugly px placement for the bottom

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,32 +6,49 @@ import Container from 'src/components/Container';
 import {HomePageProps} from 'src/interfaces/Home';
 import { IMAGE_BUTTON } from 'src/utils/Images';
 import { StyledTriangle } from 'src/styles/Main';
-import { RED } from 'src/styles/Colors';
+import { BLACK, RED, WHITE } from 'src/styles/Colors';
 import ApiQuestion from 'src/services/ApiQuestion';
 import styled from 'styled-components/native';
 
 const StyledSquare = styled.View`
+	position: absolute;
 	backgroundColor: ${RED};
 	width: 400px;
 	height: 400px;
-	position: absolute;
+	transform: rotateZ(-45deg);
+`
+
+const StyledSquareTop = styled(StyledSquare)`
 	top: -200px;
 	left: -200px;
-	z-index: 20;
-	transform: rotateZ(-45deg);
+`
+
+const StyledSquareBottom = styled(StyledSquare)`
+	bottom: -200px;
+	right: -200px;
 `
 
 const StyledSquareText = styled.Text`
 	position: absolute;
-	bottom: -40px;
-	right: -50px;
 	font-size: 30px;
 	text-transform: uppercase;
 	width: 250px;
 	text-align: center;
 	height: 40px;
 	line-height: 40px;
-	backgroundColor: yellow;
+`
+
+const StyledSquareTopText = styled(StyledSquareText)`
+	backgroundColor: ${RED};
+	bottom: -40px;
+	right: -50px;
+`
+
+const StyledSquareBottomText = styled(StyledSquareText)`
+	backgroundColor: ${BLACK};
+	color: ${WHITE};
+	top: -40px;
+	left: -50px;
 `
 
 export default class Home extends Component<HomePageProps, never> {
@@ -59,9 +76,12 @@ export default class Home extends Component<HomePageProps, never> {
 	render(): JSX.Element {
 		return (
 			<Container>
-				<StyledSquare>
-					<StyledSquareText>home</StyledSquareText>
-				</StyledSquare>
+				<StyledSquareTop>
+					<StyledSquareTopText>home</StyledSquareTopText>
+				</StyledSquareTop>
+				<StyledSquareBottom>
+					<StyledSquareBottomText>start</StyledSquareBottomText>
+				</StyledSquareBottom>
 			</Container>
 		);
 	}
