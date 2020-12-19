e6613f092aa181f382a084c19fff117c81aa3f29
Latsuj <justal.kevin@gmail.com>
2020-12-19 13:02:08 +0800

    [FEATURE] Creating the logo of the app

--- src/pages/Loading.tsx
+++ src/pages/Loading.tsx
@@ -1,15 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
+import Container from 'src/components/Container';
 import { StyleSheet, Text, View } from 'react-native';
 import ApiQuestion from 'src/services/ApiQuestion';
 import {LoadingPageProps} from 'src/interfaces/Loading';
+import styled from 'styled-components/native';
+import { LOGO } from 'src/utils/Images';
+
+const StyledImage = styled.Image`
+	height: 200px;
+	width: 200px;
+	border-bottom-right-radius: 20px;
+	border-bottom-left-radius: 20px;
+`
+
+const StyledView = styled.View`
+	flex: 1;
+	justify-content: center;
+	align-items: center;
+`
 
 export default class Loading extends Component<LoadingPageProps | LoadingPageStates> {
 
 	async componentDidMount() {
 		const result = await ApiQuestion.getAllQuestions();
 		const questions = result.get_all_questions;
-		this.props.navigation.navigate('Question', {total: this.props.total, questions: questions});
+		// this.props.navigation.navigate('Question', {total: this.props.total, questions: questions});
 	}
 
 	/**
@@ -18,9 +34,11 @@ export default class Loading extends Component<LoadingPageProps | LoadingPageSta
 	**/
 	render(): JSX.Element {
 		return (
-			<View>
-				<Text>Loading</Text>
-			</View>
+			<Container>
+				<StyledView>
+					<StyledImage source={LOGO} />
+				</StyledView>
+			</Container>
 		);
 	}
 }
--- src/utils/Images.ts
+++ src/utils/Images.ts
@@ -1,2 +1,3 @@
 export const IMAGE_BUTTON = require('src/images/button.png');
-export const BACKGROUND = require('src/images/background.jpg'); 
+export const BACKGROUND = require('src/images/background.jpg');
+export const LOGO = require('src/images/logo.png');
